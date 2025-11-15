76e471cbeeaab02666c2b06f98059e7c9c28e916
DigitalMasterworks <lukemiller@tutanota.com>
2025-11-15 07:51:27 -0600

    Delete LeanMCES directory

--- LeanMCES/LeanMCES/Bit.lean
+++
@@ -1,7 +0,0 @@
-import Std
-
-namespace LeanMCES.Bit
-abbrev Msg := List Bool
-structure KeyEnc where bits : Msg deriving Repr, DecidableEq
-structure KeyMac where bits : Msg deriving Repr, DecidableEq
-end LeanMCES.Bit
